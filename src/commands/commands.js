59724fcfa7d63c408c1504a30d7f90e900ad2096
Maçoku <rapcehermes@gmail.com>
2023-02-07 14:05:17 +0100

    Chore: update function file (js)

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,44 +1,85 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
+let config;
+let btnEvent;
 
-/* global global, Office, self, window */
+// The initialize function must be run each time a new page is loaded.
+Office.initialize = function () {};
 
-Office.onReady(() => {
-  // If needed, Office.js is ready to be called
-});
-
-/**
- * Shows a notification when the add-in command is executed.
- * @param event {Office.AddinCommands.Event}
- */
-function action(event) {
-  const message = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
-
-  // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
-
-  // Be sure to indicate when the add-in command function is complete
-  event.completed();
+function showError(error) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync(
+    "github-error",
+    {
+      type: "errorMessage",
+      message: error,
+    },
+    function (result) {}
+  );
 }
 
-function getGlobal() {
-  return typeof self !== "undefined"
-    ? self
-    : typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : undefined;
+let settingsDialog;
+
+function insertDefaultGist(event) {
+  config = getConfig();
+
+  // Check if the add-in has been configured.
+  if (config && config.defaultGistId) {
+    // Get the default gist content and insert.
+    try {
+      getGist(config.defaultGistId, function (gist, error) {
+        if (gist) {
+          buildBodyContent(gist, function (content, error) {
+            if (content) {
+              Office.context.mailbox.item.body.setSelectedDataAsync(
+                content,
+                { coercionType: Office.CoercionType.Html },
+                function (result) {
+                  event.completed();
+                }
+              );
+            } else {
+              showError(error);
+              event.completed();
+            }
+          });
+        } else {
+          showError(error);
+          event.completed();
+        }
+      });
+    } catch (err) {
+      showError(err);
+      event.completed();
+    }
+  } else {
+    // Save the event object so we can finish up later.
+    btnEvent = event;
+    // Not configured yet, display settings dialog with
+    // warn=1 to display warning.
+    const url = new URI("dialog.html?warn=1").absoluteTo(window.location).toString();
+    const dialogOptions = { width: 20, height: 40, displayInIframe: true };
+
+    Office.context.ui.displayDialogAsync(url, dialogOptions, function (result) {
+      settingsDialog = result.value;
+      settingsDialog.addEventHandler(Office.EventType.DialogMessageReceived, receiveMessage);
+      settingsDialog.addEventHandler(Office.EventType.DialogEventReceived, dialogClosed);
+    });
+  }
 }
 
-const g = getGlobal();
+// Register the function.
+Office.actions.associate("insertDefaultGist", insertDefaultGist);
 
-// The add-in command functions need to be available in global scope
-g.action = action;
+function receiveMessage(message) {
+  config = JSON.parse(message.message);
+  setConfig(config, function (result) {
+    settingsDialog.close();
+    settingsDialog = null;
+    btnEvent.completed();
+    btnEvent = null;
+  });
+}
+
+function dialogClosed(message) {
+  settingsDialog = null;
+  btnEvent.completed();
+  btnEvent = null;
+}
